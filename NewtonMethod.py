eb6199c9cae486e433b7c1b1a7546ba5d820b343
DonghwanKim <66504341+SimplePro@users.noreply.github.com>
2021-07-04 19:06:25 +0900

    to upload NewtonMethod file

--- NewtonMethod.py
+++ NewtonMethod.py
@@ -1,24 +1,6 @@
-from MathModule import Equation
+from MathModule import Equation, regex_term
 import fractions
 
-def regex_term(term, unknown_variable='x') -> str:
-    term = term.replace(" ", '')
-
-    if term[0] not in ['+', '-']: term = "+" + term
-
-    if unknown_variable not in term:
-        return term + unknown_variable + "^0"
-
-    unknown_variable_index = term.index(unknown_variable)
-
-    if term[unknown_variable_index+2:] == '':
-        term += "^1"
-
-    if term[:unknown_variable_index] in ['+', '-']:
-        term = term.replace("+", "+1")
-        term = term.replace("-", "-1")
-
-    return term
 
 def newton_method(terms = [], unknown_variable='x') -> tuple:
     terms = [regex_term(t) for t in terms]
@@ -26,19 +8,29 @@ def newton_method(terms = [], unknown_variable='x') -> tuple:
     equation = Equation(terms, unknown_variable)
     equation.to_derivative()
 
-    result_list = [1]
+    result_list = []
+
+    for i in range(1, 10000):
+        a = equation.calculate(i, mode="default") - equation.calculate(i, mode="derivative")
+        if a != 0:
+            result_list.append(i)
+            break
 
     for i in range(10000):
         result_list.append(result_list[i] - (equation.calculate(result_list[i], mode="default") / equation.calculate(result_list[i], mode="derivative")))
 
         if result_list[i-1] == result_list[i]: break
+        
 
+        # 이렇게 허근을 판단하는지는 이유는 NewtonMethodMemo 파일을 확인.
+        elif i > 3 and abs(result_list[i-1] - result_list[i-2]) <= abs(result_list[i-1] - result_list[i]): return "허근", "허근"
     
     result_list[-1] = float(str(result_list[-1])[:16])
+
     fraction = fractions.Fraction(result_list[-1])
 
     return result_list[-1], str(fraction.numerator) + '/' + str(fraction.denominator)
 
 
 if __name__ == '__main__':
-    print(newton_method(terms = ["1x^4", "-4"]))
+    print(newton_method(terms = ["x^4", "+x^3", "-x^2", "+4"]))
